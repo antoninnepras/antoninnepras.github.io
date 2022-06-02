97bdd92d6d81fbba18678f36106396626118a637
Antonin Nepras <tondanepras@gmail.com>
2022-06-02 16:27:15 +0200

    Added animations on reveal

--- script.js
+++ script.js
@@ -12,19 +12,44 @@ function reveal() {
     }
   }
 
-  var reveals_right = document.querySelectorAll(".reveal-right");
-  for (var i = 0; i < reveals_right.length; i++) {
+  var reveals_bottom = document.querySelectorAll(".reveal-bottom");
+  for (var i = 0; i < reveals_bottom.length; i++) {
     var windowHeight = window.innerHeight;
-    var elementTop = reveals_right[i].getBoundingClientRect().top;
+    var elementTop = reveals_bottom[i].getBoundingClientRect().top;
     var elementVisible = 150;
 
     if (elementTop < windowHeight - elementVisible) {
-      reveals_right[i].classList.add("active");
+      reveals_bottom[i].classList.add("active");
     } else {
-      reveals_right[i].classList.remove("active");
+      reveals_bottom[i].classList.remove("active");
     }
   }
 
+  var reveals_top = document.querySelectorAll(".reveal-top");
+  for (var i = 0; i < reveals_top.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals_top[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals_top[i].classList.add("active");
+    } else {
+      reveals_top[i].classList.remove("active");
+    }
+  }
+
+  var reveals_spin = document.querySelectorAll(".reveal-spin");
+  for (var i = 0; i < reveals_spin.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals_spin[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals_spin[i].classList.add("active");
+    } else {
+      reveals_spin[i].classList.remove("active");
+    }
+  }
 }
 
 window.addEventListener("scroll", reveal);
